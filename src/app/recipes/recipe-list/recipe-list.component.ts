cc1b3be7cac9d076b05b00a9824ba2b41299b840
stasya-sugovdz <ssugovdz.s90@gmail.com>
2018-07-19 09:45:51 +0300

    Created recipes and shopping list components and models

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test recipe', 'This is simply a test.', 'http://doingstuffoutdoors.com/wp-content/uploads/2018/04/' +
+      'fun-christmas-cake-recipe-image-url-http-keyassets-p2-timeincuk-net-wp-prod-content-uploads-sites-50-2016-07-jamaican-jpg-q-82.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
